67d9edd5e5b15d869d295bb9ff343516a401b440
Kiara Johnson <johns.kia35@gmail.com>
2022-10-12 14:54:14 -0600

    If/Else statements

--- exercises/app.js
+++ exercises/app.js
@@ -1,1 +1,39 @@
-console.log("hello world")
+console.log("hello world"
+
+let num;
+
+if (num > 0){
+    console.log("positive")
+} else if(num < 0 ) {
+    console.log("negative")
+} else {
+    console.log("not negative or positive");
+}
+
+//SOLUTION 1
+
+function test(num) {
+    if(num > 0) {
+        console.log("Positive!");
+    } else if (num < 0) {
+        console.log("Negative!");
+    } else if (num === 0) {
+        console.log("You answer is zero")
+    } else {
+        console.log("That is not a number")
+    }
+}
+
+//calling a function
+test(-5); //negative
+test(10) //calling functions again
+test(0)
+test("Dulce");
+
+const allowedAge = 21;
+
+if(allowedAge >= 18 ) {
+    console.log("Access Granted")
+} else {
+    console.log("Access Denied")
+}
